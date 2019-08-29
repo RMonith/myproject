b21f6ee484995ddab3e17d66df8905a64711e25e
rails -v <contactmonith@gmail.com>
2019-08-29 15:35:17 +0530

    first commit

---
+++ managementtool/src/main/java/com/project/managementtool/ManagementtoolApplication.java
@@ -0,0 +1,14 @@
+package com.project.managementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementtoolApplication.class, args);
+		System.out.println("Hi Started");
+	}
+
+}
